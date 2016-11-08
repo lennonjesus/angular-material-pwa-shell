fb5e9f8d50515a6565c1c68183401b67cbdf4958
Lennon Jesus <lennon.jesus@gmail.com>
2016-11-08 06:22:08 -0200

    feat: changing default color theme

--- scripts/app.config.js
+++ scripts/app.config.js
@@ -2,9 +2,15 @@
 
   'use strict';
 
-  angular.module('pwashell').config(function($mdAriaProvider) {
+  angular.module('pwashell').config(function($mdAriaProvider, $mdThemingProvider) {
     // Globally disables all ARIA warnings.
     $mdAriaProvider.disableWarnings();
+
+    // Globally set default theme colors
+    // https://material.google.com/style/color.html#color-color-palette
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue-grey')
+      .accentPalette('orange');
   });
 
 })();
